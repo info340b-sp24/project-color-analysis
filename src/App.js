47c2f9d63f20814c2f1dd56a63e81cfb9d0be4ba
Kira <kira.b@outlook.com>
2024-05-07 10:40:44 -0700

    add cards in react

--- src/App.js
+++ src/App.js
@@ -1,19 +1,22 @@
+import React from "react";
+import { CardList } from "./components/CardLists";
+
+const cardContentList = [{ card: 1, color: 'card1', stepText: 'step1', title: 'Quiz', text: 'Take the color analysis quiz, answering the questions to the best of your abilities.', step: '01' },
+{ card: 2, color: 'card2', stepText: 'step2', title: 'Results', text: 'Get your results for your best color season.', step: '02' },
+{ card: 3, color: 'card3', stepText: 'step1', title: 'Filter', text: 'Filter through products and clothing that will work best with your features.', step: '03' },
+{ card: 4, color: 'card4', stepText: 'step2', title: 'Upload', text: 'Upload products that work for you.', step: '04' }]
+
+const imgCardContent = [{card: 1, img: "img/Oil-drop.png"},
+{card: 2, img: "img/Oil-dropper.png"},
+{card: 3, img: "img/cream.png"},
+{card: 4, img: "img/hand-with-lotion.png"}]
+
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <CardList cardContent={cardContentList} cardContentImg={imgCardContent} />
+      {/* <ImgCardList cardContent={imgCardContent} /> */}
     </div>
   );
 }
