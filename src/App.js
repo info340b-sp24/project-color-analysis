2021b6fc9a5376ec36c3178d014043584365eaa9
Kira <kira.b@outlook.com>
2024-05-29 10:35:14 -0700

    add routes

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import {React} from "react";
 import { EditProfilePage } from "./components/EditProfilePage";
 import { LandingPage } from "./components/LandingPage";
 import { ProductsPage } from "./components/ProductsPage";
@@ -10,22 +10,35 @@ import QuizResultWA from './components/QuizResultWA';
 import QuizResultWS from './components/QuizResultWS';
 import QuizResultCS from './components/QuizResultCS';
 import { UploadPage } from "./components/UploadPage";
+import { Nav } from "./components/Nav";
+import { Footer } from "./components/Footer";
+// import { getDatabase, DataSnapshot, ref, push as firebasePush, onValue } from 'firebase/database';
+
+import { Route, Routes } from "react-router-dom";
+
+
 
 function App() {
+  
 
   return (
     <div>
-      {/* <LandingPage/> */}
-      <UploadPage/>
-      {/* <ProductsPage/> */}
-      {/* <ProfilePage/> */}
-      {/* <EditProfilePage/> */}
-      {/* <QuizLanding/> */}
-      {/* <QuizResultCW/> */}
-      {/* <QuizResultWA/> */}
-      {/* <QuizResultWS/> */}
-      {/* <QuizResultCS/> */}
-      {/* <QuizTaking/> */}
+      {/* <Nav/> */}
+      <Routes>
+        <Route index element={<LandingPage />} />
+        <Route path="upload" element={<UploadPage />} />
+        <Route path="products" element={<ProductsPage />} />
+        <Route path="profile" element={<ProfilePage />} />
+        <Route path="edit" element={<EditProfilePage />} />
+        <Route path="quiz" element={<QuizLanding/>} />
+
+      {/* <QuizResultCW/>
+      <QuizResultWA/>
+      <QuizResultWS/>
+      <QuizResultCS/>
+      <QuizTaking/> */}
+      </Routes>
+      {/* <Footer/> */}
     </div>
   );
 }
