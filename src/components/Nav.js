f33735632842c85d21c582ab00134ff639e713d6
Kira <kira.b@outlook.com>
2024-05-29 10:45:04 -0700

    edit nav

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,11 +1,39 @@
 import React from "react";
+import { Link } from "react-router-dom";
+
+// import Container from 'react-bootstrap/Container';
+// import Nav from 'react-bootstrap/Nav';
+// import Navbar from 'react-bootstrap/Navbar';
+// import NavDropdown from 'react-bootstrap/NavDropdown';
+
+// function BasicExample() {
+//     return (
+//         <Navbar expand="lg" className="bg-body-tertiary">
+//             <Container>
+//                 <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+//                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
+//                 <Navbar.Collapse id="basic-navbar-nav justify-content-end">
+//                     <Nav className="me-auto justify-content-end">
+//                         <Nav.Link><Link className="nav-link margin-link" aria-current="page" to="../quiz">Quiz</Link></Nav.Link>
+//                         <Nav.Link><Link className="nav-link margin-link" to="../products">Products</Link></Nav.Link>
+//                         <Nav.Link><Link className="nav-link margin-link upload-link" to="../upload">Upload</Link></Nav.Link>
+//                         <Nav.Link><Link className="nav-link" to="../profile"><span
+//                             class="material-icons profile">person</span>Profile</Link></Nav.Link>
+//                     </Nav>
+//                 </Navbar.Collapse>
+//             </Container>
+//         </Navbar>
+//     );
+// }
+
+// export default BasicExample;
 
 export function Nav() {
     return (
         <nav className="navbar navbar-expand-lg">
             <div className="container-fluid">
                 <div>
-                    <a href="index.html"><img src="img/Logo.png" alt="Color Aura logo" class="logo" /></a>
+                    <Link to=".."><img src="img/Logo.png" alt="Color Aura logo" class="logo" /></Link>
                 </div>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse"
                     data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false"
@@ -14,11 +42,11 @@ export function Nav() {
                 </button>
                 <div className="collapse navbar-collapse justify-content-end" id="navbarNavAltMarkup">
                     <div className="navbar-nav">
-                        <a className="nav-link margin-link" aria-current="page" href="quiz-landing.html">Quiz</a>
-                        <a className="nav-link margin-link" href="products.html">Products</a>
-                        <a className="nav-link margin-link upload-link" href="upload.html">Upload</a>
-                        <a className="nav-link" href="profile.html"><span
-                            class="material-icons profile">person</span>Profile</a>
+                        <Link className="nav-link margin-link" aria-current="page" to="../quiz">Quiz</Link>
+                        <Link className="nav-link margin-link" to="../products">Products</Link>
+                        <Link className="nav-link margin-link upload-link" to="../upload">Upload</Link>
+                        <Link className="nav-link" to="../profile"><span
+                            class="material-icons profile">person</span>Profile</Link>
                     </div>
                 </div>
             </div>
